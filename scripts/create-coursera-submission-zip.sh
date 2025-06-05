30da8047769fdc0b7c289bea83d3169344cb060b
nukopy <nukopy@gmail.com>
2025-06-05 16:54:17 +0900

    chore: enhance script for submissions for Coursera

--- scripts/create-coursera-submission-zip.sh
+++ scripts/create-coursera-submission-zip.sh
@@ -2,14 +2,28 @@
 
 set -euo pipefail
 
-TARGET_DIR="./projects/1"
-OUT_FILENAME="./submissions/project1.zip"
+TARGET_DIR="$1"
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <target_dir>"
+    echo "Example: $0 ./projects/1"
+    exit 1
+fi
+
+# Check if the target directory exists
+if [ ! -d "$TARGET_DIR" ]; then
+    echo "Error: Directory $TARGET_DIR does not exist." >&2
+    exit 1
+fi
+
+# Create the output filename
+# out filename: target directory "project/1" -> "project1.zip"
+OUT_FILENAME="./submissions/project$(basename "$TARGET_DIR").zip"
 
 # 出力用ディレクトリが存在しない場合は作成
 mkdir -p "$(dirname "./submissions")"
 
 # .hdl ファイルのリストを取得
-hdl_files=$(find projects/1 -type f -name "*.hdl")
+hdl_files=$(find "$TARGET_DIR" -type f -name "*.hdl")
 
 # ファイルが存在するか確認
 if [ -z "$hdl_files" ]; then
