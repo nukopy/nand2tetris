12957baceeb51555faa00199cba6959951d98481
nukopy <nukopy@gmail.com>
2025-06-16 01:15:59 +0900

    chore: add sample test

--- software/hack-assembler/src/lib.rs
+++ software/hack-assembler/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn run() {
     println!("Hello, hack-assembler!");
 }
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[test]
+fn test_add() {
+    // given
+    let a = 2;
+    let b = 3;
+    // when
+    let result = add(a, b);
+    // then
+    assert_eq!(result, 5);
+}
